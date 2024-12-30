2cd7e26360509075b17d3b14880e76ea207d1a80
HaiTao <18920079569@163.com>
2024-12-30 10:00:28 +0800

    补充说明

--- src/main/java/com/xq/tmall/service/impl/LastIDServiceImpl.java
+++ src/main/java/com/xq/tmall/service/impl/LastIDServiceImpl.java
@@ -5,7 +5,6 @@ import com.xq.tmall.service.LastIDService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import javax.annotation.Resource;
 
 @Service
 public class LastIDServiceImpl implements LastIDService {
